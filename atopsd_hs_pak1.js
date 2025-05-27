f5858a4bb4f34c826f9ed267041b7172e0847854
atozpsd <atozpsd@gmail.com>
2025-05-27 23:49:05 +0530

    Add files via upload

---
+++ atopsd_hs_pak1.js
@@ -0,0 +1,245 @@
+﻿// Sample JSON data for products
+var imgpath1 = "https://lh3.googleusercontent.com/d/";
+var imgdnldPath = "https://www.mediafire.com/file/";
+const productsData = [    {
+        "id": 1,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1KKGIPRUCVUsXXQILEagbN8UAiPKDMJEG",
+        "download": imgdnldPath + "9tz4rtdybrrz9j3/halfsaree_pack_1_atozpsd1.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 2,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1i6e3QwmMiwCc0GQ4lPAttOUAZ5Nwioax",
+        "download": imgdnldPath + "5c7xppmkoi9vjok/halfsaree_pack_1_atozpsd2.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 3,
+        "name": "Classic Bicycle",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1kOGqjnnDFobK59YCZbjPOKG0-iuwsOYh",
+        "download": imgdnldPath + "7p8jfa67i25f8c4/halfsaree_pack_1_atozpsd3.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 4,
+        "name": "1st Birthday Album Pack",
+        "description": "Premium 1st Birthday Album Pack for all your travel needs.",
+        "image": imgpath1 + "1TUMX7K8Ps2xf6aLmNVvl-OHSwTbf48mX",
+        "download": imgdnldPath + "l6xhqi48w5jt689/halfsaree_pack_1_atozpsd4.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 5,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "14jXizV6NcZlMxgiNEQugAQxX12nisQwp",
+        "download": imgdnldPath + "ejhr9vqtie3i19h/halfsaree_pack_1_atozpsd5.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    }
+    ,
+    {
+        "id": 6,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1Epf5ecmUT-YbjQlI9dGGG88HcGkMyZFd",
+        "download": imgdnldPath + "ho2rm9wwjovj0xc/halfsaree_pack_1_atozpsd6.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 7,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1eJUQIXHOl3AFtbX5P895WPDwVz9N8Tlb",
+        "download": imgdnldPath + "lmjfpvrukvot5ih/halfsaree_pack_1_atozpsd7.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },    
+    {
+        "id": 8,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1KKGIPRUCVUsXXQILEagbN8UAiPKDMJEG",
+        "download": imgdnldPath + "dg4j9yh0djwjfko/halfsaree_pack_1_atozpsd8.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 9,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "11r5qhqnkLLcPtMRM3UP7pI2Bn_HfrBJP",
+        "download": imgdnldPath + "iqlcljn8jgz5p4y/halfsaree_pack_1_atozpsd9.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 10,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1wpdeAk31-5x49evDJ4KuKsWM9nIBHihx",
+        "download": imgdnldPath + "dsfsqkqbhzo9o87/halfsaree_pack_1_atozpsd10.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 11,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1aNlWTrdYq617nFlKD-RSETJDhjH-BTgm",
+        "download": imgdnldPath + "r8q79ncp4ns576q/halfsaree_pack_1_atozpsd11.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 12,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1M3uIUuuXqKEGKiXhLm8NSpc1RupOGos_",
+        "download": imgdnldPath + "hfv2f6qmdvxezvw/halfsaree_pack_1_atozpsd12.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 13,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1gQRiWJVMrLFdQa0D88p3MfGn8Up-U-u2",
+        "download": imgdnldPath + "m34r8veaievbu2k/halfsaree_pack_1_atozpsd13.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 14,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1gbzIHeXmnf62SvAv1IO6y7tSLywg344Q",
+        "download": imgdnldPath + "49wz15huwmrycid/halfsaree_pack_1_atozpsd14.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 15,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1m1d2wD2Kk4Y_WbVweUxKsoj8cWaEQ7Zt",
+        "download": imgdnldPath + "srqjg4tmjdles60/halfsaree_pack_1_atozpsd15.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 16,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1aYx17u8tQt1McrXfWCiao7POhJvFfzx-",
+        "download": imgdnldPath + "lfv7snvc604ipw7/halfsaree_pack_1_atozpsd16.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 17,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1fP_mZRm21ZaR1OJWgLE7ai2ORq7w9V3J",
+        "download": imgdnldPath + "9jitxvbl3osem39/halfsaree_pack_1_atozpsd17.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 18,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1X98bkb-fu26H2jftwJwdedUHUsZ4KMP1",
+        "download": imgdnldPath + "bxj7j31fk1rm8hh/halfsaree_pack_1_atozpsd18.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 19,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1MPXeHYO1686HXE6kYiMm6KCpG1FKMJHp",
+        "download": imgdnldPath + "dvekp7g98wa5sxr/halfsaree_pack_1_atozpsd19.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 20,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1ySljbRzfST8YgYB6DiwOWS0myeDjPJDa",
+        "download": imgdnldPath + "3q421y8ivgou3c3/halfsaree_pack_1_atozpsd20.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 21,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1WM0uJKhWBjcsbjswR6d4tWdh3WhSfUU3",
+        "download": imgdnldPath + "6fa2ihggpmifhhs/halfsaree_pack_1_atozpsd21.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 22,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1Ar5oUVk-OnGGml-QdQCrZhDHIz_GPtPO",
+        "download": imgdnldPath + "ch7gvubv0vjcbue/halfsaree_pack_1_atozpsd22.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 23,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "13tmQpzUpRwObgQBQQ6Ap0Y2V3X2_UIWu",
+        "download": imgdnldPath + "z1z8qc0b8jwejv2/halfsaree_pack_1_atozpsd23.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 24,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1QX-XW0NHd46_27iDNXZj4SnwPLikWlSy",
+        "download": imgdnldPath + "atk6go3on22v1wm/halfsaree_pack_1_atozpsd24.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 25,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1QX-XW0NHd46_27iDNXZj4SnwPLikWlSy",
+        "download": imgdnldPath + "m3jm3t6emmyqd24/halfsaree_pack_1_atozpsd25.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 26,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1NEYrL3qc2P1YY43M-4fbRMlU_p6wcGeD",
+        "download": imgdnldPath + "98rq8ixg6zrqu0g/halfsaree_pack_1_atozpsd26.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 27,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1-polch-29nxkXLgFVga8w8LG4Dk2_bhX",
+        "download": imgdnldPath + "qgscdigm8vo3r9j/halfsaree_pack_1_atozpsd27.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 28,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1amkd5YvxX5okxmJunzpm1TTcLR_Cut08",
+        "download": imgdnldPath + "2qf6ge5bxo6uj3p/halfsaree_pack_1_atozpsd28.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 29,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1jJQedGkqIyWgRMu2RO5EZ3UlPQ_gp4cy",
+        "download": imgdnldPath + "eu1yn0uk248vi0d/halfsaree_pack_1_atozpsd29.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 30,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": imgpath1 + "1H9rQWw3URnARfCLIlIh9DtvZyXHYZKCd",
+        "download": imgdnldPath + "htqsklatlbjaj72/halfsaree_pack_1_atozpsd30.psd/file",
+        "related": [15, 13, 11, 10, 9, 8]
+    }
+];
