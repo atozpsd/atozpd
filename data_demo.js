46bd18868db97ef8a45db46b37ec903f302ee317
atozpsd <atozpsd@gmail.com>
2025-06-12 23:47:59 +0530

    Add files via upload

---
+++ data_demo.js
@@ -0,0 +1,33 @@
+﻿const atozpsdata = [{
+        "id": 1,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": gimgpth + "1KKGIPRUCVUsXXQILEagbN8UAiPKDMJEG",
+        "download": mfimgpth + "9tz4rtdybrrz9j3/halfsaree_pack_1_atozpsd1.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 2,
+        "name": "1st Birthday Album Pack",
+        "description": "1st Birthday Album Pack",
+        "image": gimgpth + "1i6e3QwmMiwCc0GQ4lPAttOUAZ5Nwioax",
+        "download": mfimgpth + "5c7xppmkoi9vjok/halfsaree_pack_1_atozpsd2.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 3,
+        "name": "Classic Bicycle",
+        "description": "1st Birthday Album Pack",
+        "image": gimgpth + "1kOGqjnnDFobK59YCZbjPOKG0-iuwsOYh",
+        "download": mfimgpth + "7p8jfa67i25f8c4/halfsaree_pack_1_atozpsd3.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    },
+    {
+        "id": 4,
+        "name": "1st Birthday Album Pack",
+        "description": "Premium 1st Birthday Album Pack for all your travel needs.",
+        "image": gimgpth + "1TUMX7K8Ps2xf6aLmNVvl-OHSwTbf48mX",
+        "download": mfimgpth + "l6xhqi48w5jt689/halfsaree_pack_1_atozpsd4.psd/file",
+        "related": [1, 8, 6, 7, 4, 3]
+    }
+];
