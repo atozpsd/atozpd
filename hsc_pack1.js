0c87543002f7ceee9cb005c233936d229dc620e2
atozpsd <atozpsd@gmail.com>
2025-07-05 18:35:46 +0530

    Add files via upload

---
+++ hsc_pack1.js
@@ -0,0 +1,274 @@
+﻿const atozpsdata = [{
+        "id": 1,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+         "image": gimgpth + "1qAQ4fFhGzuY1sNUG46pb4Y9d6q8LCl8T",
+        "download": mfimgpth + "k7ir5a2hvgwfx0e/halfsaree_pack_1_atozpsd1.psd",
+        "related": [2, 4, 6, 3]
+    },
+  /*  -------- 2 ---------*/
+    {
+        "id": 2,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1MzZ05Sdn2tzNJ4f3-4B04kXp-8VNhBLd",
+        "download": mfimgpth + "dnyup6pbyizdlaw/halfsaree_pack_1_atozpsd2.psd",
+        "related": [1, 5, 4, 6]
+    },
+    /*  -------- 3 ---------*/
+    {
+        "id": 3,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1NX4ATAp1IDO-IbEq_RKOqe5x5oDh0J1N",
+        "download": mfimgpth + "tm5njrlz2dvhuqq/halfsaree_pack_1_atozpsd3.psd",
+        "related": [2, 4, 7, 6]
+    },
+    /*  -------- 4 ---------*/
+    {
+        "id": 4,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1uAl2AR0-ZfnUzjB8Z5IqPDfSrbnj_WhS",
+        "download": mfimgpth + "vbtxi8vv9ld5kym/halfsaree_pack_1_atozpsd4.psd",
+        "related": [1, 2, 3, 5]
+    },
+
+    /*  -------- 5 ---------*/
+    {
+        "id": 5,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1JJfyIHM4wuf2H3IVQg6KibCcnbFqhgQ5",
+        "download": mfimgpth + "7xwaitufvjecbgx/halfsaree_pack_1_atozpsd5.psd",
+        "related": [1, 3, 5, 4]
+    },
+
+    /*  -------- 6 ---------*/
+    {
+        "id": 6,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1X8AAMXtri2StpSXsyo1O0vFWx9UoQg0f",
+        "download": mfimgpth + "fzodaox54afqb9v/halfsaree_pack_1_atozpsd6.psd",
+        "related": [2, 3, 5, 4]
+    },
+
+    /*  -------- 7 ---------*/
+    {
+        "id": 7,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1vZ2CNfgmy0bE7Z0rE3yAjtmg08UvDpJO",
+        "download": mfimgpth + "pchex351nes3wz9/halfsaree_pack_1_atozpsd7.psd",
+        "related": [3, 4, 5, 6]
+    },    
+    /*  -------- 8 ---------*/
+    {
+        "id": 8,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1n-3Y-cLD2KCxY7PlW-rC4aZcjx0SddI3",
+        "download": mfimgpth + "ugmup734ljidlgb/halfsaree_pack_1_atozpsd8.psd",
+        "related": [4, 6, 5, 7]
+    },
+    /*  -------- 9 ---------*/
+    {
+        "id": 9,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1a5hZQf5-oB8pYP9JuyrMpDU4u7WRqrWm",
+        "download": mfimgpth + "izpwvx9h00jf54v/halfsaree_pack_1_atozpsd9.psd",
+        "related": [5, 7, 8, 6]
+    },
+    /*  -------- 10 ---------*/
+    {
+        "id": 10,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1rzBs3ZJA129YoRXHS5dxNkRoPkl-gczX",
+        "download": mfimgpth + "v8adknnlttxrj8j/halfsaree_pack_1_atozpsd10.psd",
+        "related": [9, 8, 7, 6]
+    },
+    /*  -------- 11 ---------*/
+    {
+        "id": 11,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1d-0RsYpr1nwjI_O45suMRA2GAc4fkebl",
+        "download": mfimgpth + "y4x5bk82zdek40m/halfsaree_pack_1_atozpsd11.psd",
+        "related": [7, 8, 10, 6]
+    },
+    /*  -------- 12 ---------*/
+    {
+        "id": 12,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1FK9wkF34sMG_L1ptfgrUjFkXU_zQcc0M",
+        "download": mfimgpth + "cff91351rpkfpt0/halfsaree_pack_1_atozpsd12.psd",
+        "related": [8, 9, 11, 10]
+    },
+    /*  -------- 13 ---------*/
+    {
+        "id": 13,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1o_JVQkfAiyEvvAf00MkGlOfsH0vCK3Cm",
+        "download": mfimgpth + "duv5w5yto0tbp1c/halfsaree_pack_1_atozpsd13.psd",
+        "related": [9, 7, 10, 11]
+    },
+    /*  -------- 14 ---------*/
+    {
+        "id": 14,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1NZ-APlsLkxRjmBBt4-xOVVdmJUZFIPGp",
+        "download": mfimgpth + "39yg8bsfr9uxymc/halfsaree_pack_1_atozpsd14.psd",
+        "related": [11, 12, 14, 15]
+    },
+    /*  -------- 15 ---------*/
+    {
+        "id": 15,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "10SPxwDg5eey0oYwjQJhFfsU4scu63o1t",
+        "download": mfimgpth + "xbvfoxw7hsp59y0/halfsaree_pack_1_atozpsd15.psd",
+        "related": [12, 14, 10, 11]
+    },
+    /*  -------- 16 ---------*/
+    {
+        "id": 16,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1DlNXh22cq0v9cep1_wungUOWGvEgGq0D",
+        "download": mfimgpth + "ixoddw0dimv63es/halfsaree_pack_1_atozpsd16.psd",
+        "related": [10, 11, 12, 13]
+    },
+    /*  -------- 17 ---------*/
+    {
+        "id": 17,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1kG5nZ4Vv4psSBqLmpDgp9D65XQ7XgBkT",
+        "download": mfimgpth + "hxqgf4sqzwkhzcs/halfsaree_pack_1_atozpsd17.psd",
+        "related": [11, 13, 15, 12]
+    },
+    /*  -------- 18 ---------*/
+    {
+        "id": 18,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1gps02S9vH5klD_XOng0-40bPphkkzb4r",
+        "download": mfimgpth + "jg04r8yssbkte74/halfsaree_pack_1_atozpsd18.psd",
+        "related": [13, 15, 17, 12]
+    },
+    /*  -------- 19 ---------*/
+    {
+        "id": 19,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1Nesub2Wq03TKW9j_4b-IT9Vis0XJFeZA",
+        "download": mfimgpth + "pf16x40128205b7/halfsaree_pack_1_atozpsd19.psd",
+        "related": [12, 16, 18, 14]
+    },
+    /*  -------- 20 ---------*/
+    {
+        "id": 20,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1fYJjU_rF2HDKMX1wpo40lsJDliCGMwoQ",
+        "download": mfimgpth + "de12zax4515c95a/halfsaree_pack_1_atozpsd20.psd",
+        "related": [14, 16, 17, 19]
+    },
+    /*  -------- 21 ---------*/
+    {
+        "id": 21,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1bHAKXaV_eqRhRPz3NCAGb4Vtp0Sqxycw",
+        "download": mfimgpth + "ent0qdy8xuresnt/halfsaree_pack_1_atozpsd21.psd",
+        "related": [16, 15, 18, 19]
+    },
+    /*  -------- 22 ---------*/
+    {
+        "id": 22,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1TS-2GlbpuXI8YIs7_UV5Hvj-8vWIwDt6",
+        "download": mfimgpth + "p4p67l86pjasnma/halfsaree_pack_1_atozpsd22.psd",
+        "related": [16, 18, 22, 24]
+    },
+    /*  -------- 23 ---------*/
+    {
+        "id": 23,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1ze4XKzV0Ek0ufPGpyzoHycPP0nwUWETH",
+        "download": mfimgpth + "tp8y40u4y9o8zbl/halfsaree_pack_1_atozpsd23.psd",
+        "related": [17, 19, 22, 24]
+    },
+    /*  -------- 24 ---------*/
+    {
+        "id": 24,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1jLTTvFywn5V8gkIpcppfxUJgcC8itSex",
+        "download": mfimgpth + "9l74l1buf8hois3/halfsaree_pack_1_atozpsd24.psd",
+        "related": [20, 19, 20, 23]
+    },
+    /*  -------- 25 ---------*/
+    {
+        "id": 25,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1QyvyyobQ6ZGQ3kdoPU8nseZ9cRWrQ3Kp",
+        "download": mfimgpth + "pcshui21woy7zwn/halfsaree_pack_1_atozpsd25.psd",
+        "related": [20, 21, 19, 18]
+    },
+    /*  -------- 26 ---------*/
+    {
+        "id": 26,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1gNKZ1vQD2wuE9IUrOJtXyI4vAOTkkkj1",
+        "download": mfimgpth + "bwsnz0i2xru9ss0/halfsaree_pack_1_atozpsd26.psd",
+        "related": [22, 20, 24, 19]
+    },
+    /*  -------- 27 ---------*/
+    {
+        "id": 27,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1HqHYHV7bG590JikY6c1Fm-im5qUrPQAl",
+        "download": mfimgpth + "rcrmfji61ryumu5/halfsaree_pack_1_atozpsd27.psd",
+        "related": [25, 23, 20, 21]
+    },
+    /*  -------- 28 ---------*/
+    {
+        "id": 28,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1DHWwRnc6U-idYpUcIlWZTmnrb14zsnmU",
+        "download": mfimgpth + "4ip2dn304azdr6p/halfsaree_pack_1_atozpsd28.psd",
+        "related": [21, 26, 22, 25]
+    },
+    /*  -------- 29 ---------*/
+    {
+        "id": 29,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "1hfLx2xyZwzdJOjbJupaJQF1L4pUfUPCx",
+        "download": mfimgpth + "jlm3gu7868t8ov4/halfsaree_pack_1_atozpsd29.psd",
+        "related": [19, 23, 25, 28]
+    },
+    /*  -------- 30 ---------*/
+    {
+        "id": 30,
+        "name": "Atozpsd Half Saree Pack 1",
+        "description": "Atozpsd Half Saree Pack 1 - Download for FREE",
+        "image": gimgpth + "17LShy72CFqoL0XOaDZJvaPkWQAvJ2Z_K",
+        "download": mfimgpth + "500638j5db1g4pe/halfsaree_pack_1_atozpsd30.psd",
+        "related": [20, 24, 19, 21]
+    }
+   
+];
