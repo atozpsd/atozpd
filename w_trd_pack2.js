58e62e52c07d734503d945a8e5877511ee79fd4d
atozpsd <atozpsd@gmail.com>
2025-07-12 21:24:33 +0530

    Add files via upload
    Free Traditional Pack 2

---
+++ w_trd_pack2.js
@@ -0,0 +1,273 @@
+﻿const atozpsdata = [{
+    "id": 1,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1DR2OGjERJL3d0frmPNJCGBVyOoQAhhBb",
+    "download": mfimgpth + "scatt2a3j1gqroy/atozpsd_traditional_pack2%252801%2529.rar",
+    "related": [2, 4, 6, 3]
+},
+/*  -------- 2 ---------*/
+{
+    "id": 2,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1NwJKoJ8u2zvxwLQoY9FwtQz1tnk57U16",
+    "download": mfimgpth + "3b9k7mg0dqeoj3w/atozpsd_traditional_pack2%252802%2529.rar",
+    "related": [1, 5, 4, 6]
+},
+/*  -------- 3 ---------*/
+{
+    "id": 3,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1UhXPJ3ISczVALApSUz-kjZU-gMrp9im3",
+    "download": mfimgpth + "fd5q2f0wnlo21e3/atozpsd_traditional_pack2%252803%2529.rar",
+    "related": [2, 4, 7, 6]
+},
+/*  -------- 4 ---------*/
+{
+    "id": 4,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1b3TKscO6YfVZBoqZxTyKj4jafx524iDJ",
+    "download": mfimgpth + "8w76g59k8iwqsoh/atozpsd_traditional_pack2%252804%2529.rar",
+    "related": [1, 2, 3, 5]
+},
+
+/*  -------- 5 ---------*/
+{
+    "id": 5,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1EXmEFHaKpPWR2gY8cFzVVS5W65JKkMSe",
+    "download": mfimgpth + "dy3n5clafmuttld/atozpsd_traditional_pack2%252805%2529.rar",
+    "related": [1, 3, 5, 4]
+},
+
+/*  -------- 6 ---------*/
+{
+    "id": 6,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1az2e09YIHMaEisDxA3s9Ab7CRolb6cyH",
+    "download": mfimgpth + "y5bgy729gthcp15/atozpsd_traditional_pack2%252806%2529.rar",
+    "related": [2, 3, 5, 4]
+},
+
+/*  -------- 7 ---------*/
+{
+    "id": 7,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1k1gXRLU1dGWMgAE630g3aT-60FnRF-AT",
+    "download": mfimgpth + "9udqqhgnof5y6kn/atozpsd_traditional_pack2%252807%2529.rar",
+    "related": [3, 4, 5, 6]
+},
+/*  -------- 8 ---------*/
+{
+    "id": 8,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1w6tIIW8xN-6KLmnWmo89GFmWXJDs9bFU",
+    "download": mfimgpth + "22gmwnatmnhhjds/atozpsd_traditional_pack2%252808%2529.rar",
+    "related": [4, 6, 5, 7]
+},
+/*  -------- 9 ---------*/
+{
+    "id": 9,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1ltIji8DQgxBbKnUqyZdAgv9oZGyqv5Ae",
+    "download": mfimgpth + "wa3zh4ds9xexkav/atozpsd_traditional_pack2%252809%2529.rar",
+    "related": [5, 7, 8, 6]
+},
+/*  -------- 10 ---------*/
+{
+    "id": 10,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1QsieJETjO_Lio5CjawZb3G5QbIbWC3hi",
+    "download": mfimgpth + "q0gi13cej2th7dr/atozpsd_traditional_pack2%252810%2529.rar",
+    "related": [9, 8, 7, 6]
+},
+/*  -------- 11 ---------*/
+{
+    "id": 11,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1NGtZyryf3NOk0JcvJ3Vkj2oDZygXfl5k",
+    "download": mfimgpth + "18gs5rr7qbvgwyh/atozpsd_traditional_pack2%252811%2529.rar",
+    "related": [7, 8, 10, 6]
+},
+/*  -------- 12 ---------*/
+{
+    "id": 12,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1tFhU89VgyA30LgvJIT_3owZKAUW9I6kI",
+    "download": mfimgpth + "qxhl1897m5bnk3r/atozpsd_traditional_pack2%252812%2529.rar",
+    "related": [8, 9, 11, 10]
+},
+/*  -------- 13 ---------*/
+{
+    "id": 13,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "10K4pqqzC63ThCv0Fdia__j4Bxwtpi_js",
+    "download": mfimgpth + "4s58272se9l74au/atozpsd_traditional_pack2%252813%2529.rar",
+    "related": [9, 7, 10, 11]
+},
+/*  -------- 14 ---------*/
+{
+    "id": 14,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1g54WxtdQ5HTgqZ_799M8uov96mKzJQ0R",
+    "download": mfimgpth + "wr6s8de3faajwci/atozpsd_traditional_pack2%252814%2529.rar",
+    "related": [11, 12, 14, 15]
+},
+/*  -------- 15 ---------*/
+{
+    "id": 15,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1svMgz3T7NiNeJnGAn14al-oxTA1QmgOz",
+    "download": mfimgpth + "yqoqra7ev81arqd/atozpsd_traditional_pack2%252815%2529.rar",
+    "related": [12, 14, 10, 11]
+},
+/*  -------- 16 ---------*/
+{
+    "id": 16,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1HJsMgPaxcy-e5ySP1UujBlnpoSZKmjhr",
+    "download": mfimgpth + "s8qmiwqzx6i549g/atozpsd_traditional_pack2%252816%2529.rar",
+    "related": [10, 11, 12, 13]
+},
+/*  -------- 17 ---------*/
+{
+    "id": 17,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1kKnzGsUDhNEpKo0gEQWF0bokACB8mvpV",
+    "download": mfimgpth + "px336wm9njtqwiq/atozpsd_traditional_pack2%252817%2529.rar",
+    "related": [11, 13, 15, 12]
+},
+/*  -------- 18 ---------*/
+{
+    "id": 18,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1mBV50yrqzItTtbUtxUuwZu2W9vetXUhl",
+    "download": mfimgpth + "uixj5jcbudk9kuy/atozpsd_traditional_pack2%252818%2529.rar",
+    "related": [13, 15, 17, 12]
+},
+/*  -------- 19 ---------*/
+{
+    "id": 19,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1RM3yWPhgbdETEOC-7PAv1cY-yVgBQ6Gq",
+    "download": mfimgpth + "26xhsse6v933eew/atozpsd_traditional_pack2%252819%2529.rar",
+    "related": [12, 16, 18, 14]
+},
+/*  -------- 20 ---------*/
+{
+    "id": 20,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1ri4Av_928z9W-Y1xLx6e3X0_sUucTK-R",
+    "download": mfimgpth + "vpxdqecghwlqtvk/atozpsd_traditional_pack2%252820%2529.rar",
+    "related": [14, 16, 17, 19]
+},
+/*  -------- 21 ---------*/
+{
+    "id": 21,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1xmNHsP-G8xSmLfGkeNXWPhMsnfun6wd1",
+    "download": mfimgpth + "y6cqqp9yp2vf35p/atozpsd_traditional_pack2%252821%2529.rar",
+    "related": [16, 15, 18, 19]
+},
+/*  -------- 22 ---------*/
+{
+    "id": 22,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1xFZr56bKmTGEIoxvYQPgSkEGjBM9hsPK",
+    "download": mfimgpth + "06xkz8cyq9q9tiv/atozpsd_traditional_pack2%252822%2529.rar",
+    "related": [16, 18, 22, 24]
+},
+/*  -------- 23 ---------*/
+{
+    "id": 23,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1cBdTNV974RnpLPjSo1QZft68JaXxDYKK",
+    "download": mfimgpth + "1j14kzpfiln2ci2/atozpsd_traditional_pack2%252823%2529.rar",
+    "related": [17, 19, 22, 24]
+},
+/*  -------- 24 ---------*/
+{
+    "id": 24,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1FQcHLWvszj2pFq9uIHqs_cWg1JvrkkSR",
+    "download": mfimgpth + "f9ttvxeo8buuc74/atozpsd_traditional_pack2%252824%2529.rar",
+    "related": [20, 19, 20, 23]
+},
+/*  -------- 25 ---------*/
+{
+    "id": 25,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1eVf4QYT8SJ2uR6F68_yTSRpa4CrWQJey",
+    "download": mfimgpth + "eireqwqlzibpr6t/atozpsd_traditional_pack2%252825%2529.rar",
+    "related": [20, 21, 19, 18]
+},
+/*  -------- 26 ---------*/
+{
+    "id": 26,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1LN421W74i4_1aZBhUXuj9YBzoxobvEzx",
+    "download": mfimgpth + "bpjkdm4ck1phb9c/atozpsd_traditional_pack2%252826%2529.rar",
+    "related": [22, 20, 24, 19]
+},
+/*  -------- 27 ---------*/
+{
+    "id": 27,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1aYoYND6dGcNGzJKKW3OHIsQZXjO4zo3m",
+    "download": mfimgpth + "dwwchx2prnh9hn7/atozpsd_traditional_pack2%252827%2529.rar",
+    "related": [25, 23, 20, 21]
+},
+/*  -------- 28 ---------*/
+{
+    "id": 28,
+    "name": "Wedding Traditional Pack 1",
+    "description": "Wedding Traditional Pack 1 - Download for FREE",
+    "image": gimgpth + "1L6AWS2Fc7cVvGGgdm37fwg5JNzSMA28F",
+    "download": mfimgpth + "ceaasvldge5c0i7/atozpsd_traditional_pack2%252828%2529.rar",
+    "related": [21, 26, 22, 25]
+},
+/*  -------- 29 ---------*/
+//{
+//    "id": 29,
+//    "name": "Wedding Traditional Pack 1",
+//    "description": "Wedding Traditional Pack 1 - Download for FREE",
+//    "image": gimgpth + "1ESQwXLAR6dl-PPMALY0_m3EKBE0GiYaF",
+//    "download": mfimgpth + "wcvwz1gvc6gi5sv/atozpsd_traditional_pack2%252829%2529.rar",
+//    "related": [19, 23, 25, 28]
+//},
+/*  -------- 30 ---------*/
+//{
+//    "id": 30,
+//    "name": "Wedding Traditional Pack 1",
+//    "description": "Wedding Traditional Pack 1 - Download for FREE",
+//    "image": gimgpth + "1b1-Goa2cuDkIthkFjHtBjY8QQTGpaYU4",
+//    "download": mfimgpth + "eosp45h68tdvnii/atozpsd_traditional_pack2%252830%2529.rar",
+//    "related": [20, 24, 19, 21]
+//    }
+];
